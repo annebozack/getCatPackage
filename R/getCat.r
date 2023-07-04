2e26303aa2a115a60bb3fc9bd69aa8bc7f0d9672
annebozack <anne.bozack@gmail.com>
2023-07-03 23:08:40 -0400

    adding options

--- R/getCat.r
+++ R/getCat.r
@@ -1,17 +1,24 @@
 #' Get cat function
 #'
 #' This function will get a cat for you.
+#' @param cat Logical. If true a cat is generated; if false a dog is generated.
 #' @keywords cats
 #' @export
 #' @examples
 #' getCat()
 
-getCat = function(){
+getCat = function(cat = TRUE){
     cat('|\\---/|')
-    draw = T
+    draw = TRUE
+
+    if (cat == TRUE){
+        url = 'https://api.thecatapi.com/v1/images/search'
+    } else {
+        url = 'https://api.thedogapi.com/v1/images/search'
+    }
 
     while(draw){
-        catjson = httr::GET('https://api.thecatapi.com/v1/images/search')
+        catjson = httr::GET(url)
         catdata = jsonlite::fromJSON(rawToChar(catjson$content))
 
         type = stringr::str_sub(catdata$url, start = length(catdata$url) - 4)
@@ -24,19 +31,23 @@ getCat = function(){
     }
     cat("\n| o_o |")
 
-    download.file(catdata$url,catdata$id, mode = 'wb', quiet=T)
+    download.file(catdata$url,catdata$id, mode = 'wb', quiet=TRUE)
 
     if (type == 'jpg'){
-        cat = jpeg::readJPEG(catdata$id, native = T)
+        catimg = jpeg::readJPEG(catdata$id, native = T)
     } else {
-        cat = png::readPNG(catdata$id, native = T)
+        catimg = png::readPNG(catdata$id, native = T)
     }
-    cat("\n \\_^_/ ")
+    if (cat == TRUE){
+        cat("\n \\_^_/ ")
+    } else {
+        cat("\n \\_O_/ ")
+    }
 
     plot(0:1, 0:1, type="n", ann=FALSE, axes=FALSE) 
     ratio = catdata$height/catdata$width
-    rasterImage(cat, 0, 0, 1, ratio)
+    rasterImage(catimg, 0, 0, 1, ratio)
 
-    file.remove(catdata$id)
+    invisible(file.remove(catdata$id))
 }
 
